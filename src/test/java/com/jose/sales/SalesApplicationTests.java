d3f73b1041ab367715483743f0d596fd7d18c664
Dakson38 <daksonvibes38@gmail.com>
2025-03-12 20:41:08 -0400

    style: adding prettier

--- src/test/java/com/jose/sales/SalesApplicationTests.java
+++ src/test/java/com/jose/sales/SalesApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SalesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }

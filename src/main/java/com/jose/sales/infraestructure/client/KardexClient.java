b3241105dcc14f9e21feb3da7e7b7cbc5429243f
Dakson38 <daksonvibes38@gmail.com>
2025-03-17 16:34:12 -0400

    chore: change response type KardexClient

--- src/main/java/com/jose/sales/infraestructure/client/KardexClient.java
+++ src/main/java/com/jose/sales/infraestructure/client/KardexClient.java
@@ -1,6 +1,7 @@
 package com.jose.sales.infraestructure.client;
 
 import com.jose.sales.api.model.request.CreateKardexRequest;
+import com.jose.sales.api.model.response.CreatedKardexResponse;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "msvc-kardex")
 public interface KardexClient {
   @PostMapping(path = "/kardex")
-  CreateKardexRequest saveSaleIntoKardex(
+  CreatedKardexResponse saveSaleIntoKardex(
     @RequestBody List<CreateKardexRequest> request
   );
 }
